f936204f2c018c7b9b02dbfabfd6dc8f384224cb
zsw12013 <zsw12013@126.com>
2016-05-29 21:58:32 +0800

    test

--- src/test/java/net/mingsoft/Test.java
+++ src/test/java/net/mingsoft/Test.java
@@ -3,5 +3,7 @@ package net.mingsoft;
 import com.mingsoft.base.test.BaseTest;
 
 public class Test extends BaseTest{
-
+  public static void main(String[] args) {
+	System.out.println("test");
+  }
 }
